8a49afd385b488ebe712997f9cd6838dc764f21f
pratik-204 <85887097+pratik-204@users.noreply.github.com>
2023-04-05 17:55:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAWvigo5up1PayfHSoMbIRmKSFj9PWPWJI",
+  authDomain: "story-telling-app-3dfee.firebaseapp.com",
+  databaseURL: "https://story-telling-app-3dfee-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-3dfee",
+  storageBucket: "story-telling-app-3dfee.appspot.com",
+  messagingSenderId: "530848713688",
+  appId: "1:530848713688:web:eee164c5844c58e9d000ad"
+};
